0c43fe62cc4fa6123f386eae8664256ad4ff46a8
Matt Kelam <maaronkel@protonmail.com>
2020-05-19 15:37:59 +1000

    Uploaded all page layouts

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// Smooth Scroll effect - https://github.com/cferdinandi/smooth-scroll
+var scroll = new SmoothScroll('.scroll a[href*="#"]', {
+  speed: 900
+});
+
+// Light / Dark toggle - https://www.florin-pop.com/blog/2019/05/dark-light-theme-toggle/
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('input', e => {
+  const isChecked = e.target.checked;
+
+  if (isChecked) {
+    body.classList.add('dark-theme');
+  } else {
+    body.classList.remove('dark-theme');
+  }
+});
